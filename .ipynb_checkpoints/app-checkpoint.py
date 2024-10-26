1c636496277d61ca710dc14ad4cc9718e74b4858
Narayana Kadoor <44755592+nk2001@users.noreply.github.com>
2024-10-25 18:44:59 -0700

    Model update with latest APIs

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,24 @@
+#import numpy as np
+import streamlit as st 
+from PIL import Image
+from classify import identify
+from tempfile import NamedTemporaryFile
+from keras.preprocessing.image import load_img
+
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+st.title("Identifying if a leaf is healthy")
+uploaded_file = st.file_uploader("Upload an image of a leaf in jpg format ...", type="jpg")
+temp_file = NamedTemporaryFile(delete=False)
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+	#Use this trick to overcome streamit file upload problem discussed here: https://discuss.streamlit.io/t/image-upload-problem/4810
+    temp_file.write(uploaded_file.getvalue())
+	
+    st.write("")
+    st.subheader("Checking the Image...")
+    label = identify(temp_file.name)
+    st.subheader(label)
---
+++ .ipynb_checkpoints/classify-checkpoint.py
@@ -0,0 +1,32 @@
+from keras.preprocessing import image
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.resnet50 import preprocess_input
+from keras.applications.resnet50 import decode_predictions
+from PIL import Image
+import numpy as np
+from keras.models import load_model
+
+
+def identify(image1): 
+    model = load_model("model_MobileNetv2.h5")
+    #Resize the image to 224x224
+    image = load_img(image1, target_size=(224, 224))
+    #convert the image pixels to a numpy array
+    image = img_to_array(image)
+    # reshape data for the model to add another dimension as keras wants the input as set of images which have 3D
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+
+    # prepare the image for the model
+    image = preprocess_input(image)
+    # predict the probability across all output classes
+    pred = model.predict(image)
+    # convert the probabilities to the class labels
+    label = np.argmax(pred, axis = 1)
+	# retrieve the most likely result, e.g. highest probability
+    label = label[0]
+    if label:
+	    label1 = "HEALTHY"
+    else:
+	    label1 = "NOT HEALTHY"
+    return label1 
